35c3f8426bd78a4a1707637383f0ff1fcdfbad45
amine affif <affif.amine@live.fr>
2020-10-17 18:29:48 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDUtuhJ5iy_5vGAae7K6ofma7wPqV2B2UA",
+    authDomain: "clone-dc2bb.firebaseapp.com",
+    databaseURL: "https://clone-dc2bb.firebaseio.com",
+    projectId: "clone-dc2bb",
+    storageBucket: "clone-dc2bb.appspot.com",
+    messagingSenderId: "994927248523",
+    appId: "1:994927248523:web:b0b2f36783647d0241808e",
+    measurementId: "G-W5DDJ4DKHV"
+};
