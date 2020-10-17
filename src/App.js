86860edfca01c553bdddb8cc15e9fd829108efaf
amine affif <affif.amine@live.fr>
2020-10-17 19:44:07 +0200

    UI header component finished(logo + search bar + nav options + shopping cart)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-
+      <Header/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './css/Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <img className='header__logo' src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt="amazon-header-logo"/>
+            <div className='header__search'>
+                <input className='header__searchInput' type="text"/>
+                <SearchIcon className='header__searchIcon'/>
+            </div>
+            <div className='header__nav'>
+
+                <div className='header__navOption'>
+                    <span className='header__optionLineOne'>
+                        Hello guest
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        Sign in
+                    </span>
+                </div>
+
+                <div className='header__navOption'>
+                    <span className='header__optionLineOne'>
+                        Returns
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        & Orders
+                    </span>
+                </div>
+
+                <div className='header__navOption'>
+                    <span className='header__optionLineOne'>
+                        Your
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        Prime
+                    </span>
+                </div>
+
+                <div className='header__navOptionCart'>
+                    <ShoppingBasketIcon/>
+                    <span className='header__optionLineTwo headerBasketCount'>
+                        0
+                    </span>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
+
