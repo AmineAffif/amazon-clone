96c53a889372e4ebc172bfb9b4a600c12d818e16
amine affif <affif.amine@live.fr>
2020-10-17 20:08:17 +0200

    added the top-body ads-image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './css/App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Home/>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/Home.css';
+
+const Home = () => {
+    return (
+        <div className='home__container'>
+            <img className='home__image' src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg" alt="home-image-slide-ads-for-amazon"/>
+        </div>
+    );
+};
+
+export default Home;
