4edd27a5a865511551716189f18337811a589702
amine affif <affif.amine@live.fr>
2020-10-18 20:21:55 +0200

    now the BackToTop btn works

--- src/Footer.js
+++ src/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './css/Footer.css';
 
+
+
 const Footer = () => {
+
     return (
         <div className='footer'>
             <div className='footer__container'>
-                <span>Back to top</span>
+                <span><a href="#top" id='toTopA'>Back to top</a></span>
             </div>
+
         </div>
     );
 };
-
 export default Footer;
--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ import Product from "./Product";
 const Home = () => {
     return (
         <div className='home__container'>
+            <a name="top" id='top_anchor'></a>
             <img className='home__image' src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg" alt="home-image-slide-ads-for-amazon"/>
             <div className='home__rowsContainer'>
                 <div className='home__row'>
