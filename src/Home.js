984cdbd6500159f2fd2d116b5d487f4e5a3c7fd8
amine affif <affif.amine@live.fr>
2020-10-18 19:34:05 +0200

    placed all the products (same product for now) in every row

--- src/Home.js
+++ src/Home.js
@@ -6,25 +6,27 @@ const Home = () => {
     return (
         <div className='home__container'>
             <img className='home__image' src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg" alt="home-image-slide-ads-for-amazon"/>
-            <div className='home__row'>
-                <Product/>
-                <Product/>
-                <Product/>
-            </div>
-            <div className='home__row'>
-                {/* product */}
-                {/* product */}
-            </div>
-            <div className='home__row'>
-                {/* product */}
-                {/* product */}
-                {/* product */}
-                {/* product */}
-            </div>
-            <div className='home__row'>
-                {/* product */}
-                {/* product */}
-                {/* product */}
+            <div className='home__rowsContainer'>
+                <div className='home__row'>
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                </div>
+                <div className='home__row'>
+                    <Product/>
+                    <Product/>
+                </div>
+                <div className='home__row'>
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                </div>
+                <div className='home__row'>
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                </div>
             </div>
         </div>
     );
