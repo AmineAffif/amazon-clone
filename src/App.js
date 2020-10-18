c496c9baa4e4c618727b2d3547f2c2633e51b511
amine affif <affif.amine@live.fr>
2020-10-18 19:46:01 +0200

    added a footer component, and added a div to clic in order to go back to top of the page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './css/App.css';
 import Header from './Header';
 import Home from './Home';
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="app">
       <Header/>
       <Home/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='footer__container'>
+                <span>Back to top</span>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
